3177ca9299bf5327f0ca0169bf76d167650e9183
Abhishek <aabhishekbbhardwaj@gmail.com>
2022-05-23 08:51:57 +0530

    Improvements For Anil

---
+++ client/src/components/views/NavBar/Sections/RightMenu.js
@@ -0,0 +1,67 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import { Menu, Icon, Badge } from 'antd';
+import axios from 'axios';
+import { USER_SERVER } from '../../../Config';
+import { withRouter } from 'react-router-dom';
+import { useSelector } from "react-redux";
+
+function RightMenu(props) {
+  const user = useSelector(state => state.user);
+  const userId = '628af295207333450466c882';
+  const localUserId = localStorage.getItem('userId')
+  const logoutHandler = () => {
+    axios.get(`${USER_SERVER}/logout`).then(response => {
+      if (response.status === 200) {
+        props.history.push("/login");
+      } else {
+        alert('Log Out Failed')
+      }
+    });
+  };
+
+  if (user.userData && !user.userData.isAuth) {
+    return (
+      <Menu mode={props.mode}>
+        <Menu.Item key="mail">
+          <a href="/login">Signin</a>
+        </Menu.Item>
+        <Menu.Item key="app">
+          <a href="/register">Signup</a>
+        </Menu.Item>
+      </Menu>
+    )
+  } else {
+    return (
+      <Menu mode={props.mode}>
+
+        <Menu.Item key="history">
+          <a href="/history">History</a>
+        </Menu.Item>
+
+        <Menu.Item key="upload" hidden={!(userId == localUserId)}>
+          <a href="/product/upload">Upload</a>
+        </Menu.Item>
+
+        <Menu.Item key="cart" style={{ paddingBottom: 3 }}>
+          <Badge count={user.userData && user.userData.cart.length}>
+            <a href="/user/cart" style={{ marginRight: -22 , color:'#667777'}}>
+              <Icon type="shopping-cart" style={{ fontSize: 30, marginBottom: 3 }} />
+            </a>
+          </Badge>
+        </Menu.Item>
+        
+        {/* <Menu.Item key="user_img" hidden={!(userId == localUserId)}>
+          <a style={{fontWeight:'bold',height:'30px',width:'30px',borderRadius:'10px', backgroundColor:'yellow'}}>A</a>
+        </Menu.Item> */}
+
+        <Menu.Item key="logout">
+          <a onClick={logoutHandler}>Logout</a>
+        </Menu.Item>
+      </Menu>
+    )
+  }
+}
+
+export default withRouter(RightMenu);
+
